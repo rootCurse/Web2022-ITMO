797676c4f598873dbe8a8558eb080b67bef9f31e
rootCurse <58246636+rootCurse@users.noreply.github.com>
2022-12-08 23:15:09 +0300

    fix: add template, styles and input

--- btnHandler.js
+++ btnHandler.js
@@ -1,12 +1,24 @@
 document.addEventListener('click', function(evt){
     if(evt.target.matches('.btn__add')){
         evt.preventDefault();
-        var element = evt.target;
-        var result = prompt('Введите название', 'unknown');
-        if (result === null) return;
-        var container = element.parentElement.parentElement;
-        task = `<div class='task__name'>` + result + `</div>`
-        container.innerHTML += `<div class = 'task'> ` + task + `<button class='btn__delete'></button></div>`;
+        let input = document.querySelector('#form__input');
+        let popup = document.querySelector('div.parent__popup_invisible');
+        popup.className = "parent__popup_visible";
+        let element = evt.target;
+        input.addEventListener('submit', function(evt_sub){
+            evt_sub.preventDefault();
+            let data = new FormData(input);
+            if (data.get('task__title') === ''){
+                popup.className = "parent__popup_invisible";
+                return;
+            }
+            let container = element.parentElement.parentElement;
+            let templ = document.querySelector("#template__table_task");
+            let newNode = document.importNode(templ.content, true);
+            newNode.querySelector('.task__name').textContent = data.get('task__title');
+            container.appendChild(newNode);
+            popup.className = "parent__popup_invisible";
+        })
     }
     if(evt.target.matches('.btn__delete')) {
         evt.preventDefault();
--- constructor.js
+++ constructor.js
@@ -1,17 +1,23 @@
 const f = document.querySelector('#create__table');
 f.addEventListener('submit', function(evt){
     evt.preventDefault();
-    const m = document.querySelector('main');
+    const wrapper = document.querySelector('div.wrapper');
     const week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     let data = new FormData(f);
-    m.innerHTML = `<div class='table__title'>` + data.get('table__title') + `</div>`;
-    let content = `<grid class='table' id = 'table'>`;
+    wrapper.innerHTML = `<div class='table__title'>` + data.get('table__title') + `</div>`;
+    wrapper.innerHTML += `<grid class='table' id = 'table'></grid>`;
+    let templ = document.querySelector("#template__table_column");
     let gap = '';
     for(let i = 0; i < data.get('days'); i++){
-        content += `<div class = 'grid__element'><div class='grid__element_title'><h3>`+ week[i] + `</h3><button class='btn__add'></button></div></div>`;
+        let newNode = document.importNode(templ.content, true);
+        newNode.querySelector('h3.column__title').textContent = week[i];
+        table.appendChild(newNode);
         gap += '1fr ';
     }
-    content += `</div>`;
-    m.innerHTML += content;
+    formInput = `<div class="parent__popup_invisible"><form id="form__input" name="form__input" class="form__input_invisible"><input type="text" name="task__title" id="task__title" placeholder="Name of your task"></input><button type="submit" id="create__task">Create task</button></form></div>`
+    wrapper.innerHTML += formInput;
     table.style.gridTemplateColumns = gap;
+    localStorage.clear();
+    localStorage.setItem('table__title', data.get('table__title'))
+    localStorage.setItem('table__days', data.get('days'))
 });
